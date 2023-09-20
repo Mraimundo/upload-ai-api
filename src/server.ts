638873b96ff1b03b98289e0fb1793aa628d217fd
Mraimundo <mdumundo18@gmail.com>
2023-09-19 22:23:04 -0300

    feat: add route upload video

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
 import { fastify } from "fastify";
+import { getAllPromptsRoute } from "./routes/get-all-prompts";
+import { uploadVideosRoute } from "./routes/upload-video";
 
 const app = fastify();
 
-app.get("/", () => {
-  return "Hello Word";
-});
+app.register(getAllPromptsRoute);
+app.register(uploadVideosRoute);
 
 app
   .listen({
